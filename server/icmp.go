50c2a12e274e13c0121d1b5b5144851bd4873dc3
hasbai <jsclndnz@outlook.com>
2023-02-14 13:00:19 +0800

    fix icmp packet parse

--- server/icmp.go
+++ server/icmp.go
@@ -34,18 +34,18 @@ func serveICMP() {
 
 func handleICMP(conn *ipv6.PacketConn) error {
 	buf := make([]byte, 64)
-	n, _, addr, err := conn.ReadFrom(buf)
+	_, _, addr, err := conn.ReadFrom(buf)
 	if err != nil {
 		return err
 	}
 
 	start := time.Now()
 
-	icmpMsg, err := icmp.ParseMessage(ProtocolIPv6ICMP, buf[:n])
-	if err != nil {
-		return err
-	}
-	if icmpMsg.Type != ipv6.ICMPTypeEchoRequest { // only accept ping
+	// It's hard to get ipv6 header(i.e. destination address) from raw socket
+	// see: RFC3542 https://www.rfc-editor.org/rfc/rfc3542
+	// so we turn to check the ping packet returned by our machine
+	// and the address is available as the source address
+	if ipv6.ICMPType(buf[0]) != ipv6.ICMPTypeEchoReply { // first byte is icmp type
 		return nil
 	}
 
@@ -54,7 +54,10 @@ func handleICMP(conn *ipv6.PacketConn) error {
 		return errors.New(fmt.Sprintf("addr %s is not an IPAddr", addr))
 	}
 
-	pixel := lib.IP2Pixel(ipAddr.IP)
+	pixel, err := lib.IP2Pixel(ipAddr.IP)
+	if err != nil {
+		return nil
+	}
 	modifyImage(pixel)
 
 	log.Printf("%s %dμs", addr, time.Since(start).Microseconds())
